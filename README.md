ceb0cb12619fafad50a3d9eb34be10a46d9987a8
contracode <me@contracode.com>
2024-11-03 19:22:32 -0500

    Add ACME DNS-o1 authentication and cleanup scripts

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 <img alt="GitHub" src="https://img.shields.io/github/license/contracode/cloudflare-dns-challenge-hooks?color=black"> <img alt="GitHub last commit (branch)" src="https://img.shields.io/github/last-commit/contracode/cloudflare-dns-challenge-hooks/main"> <img alt="GitHub contributors" src="https://img.shields.io/github/contributors/contracode/cloudflare-dns-challenge-hooks">
 
-This script implements support for the Automatic Certificate Management Environment ([ACME](https://en.wikipedia.org/wiki/Automatic_Certificate_Management_Environment) protocol's [DNS-01](https://letsencrypt.org/docs/challenge-types/#dns-01-challenge) challenge. In this scenario, the domain owner creates a DNS TXT record containing a specific token for the domain. Following this, the Certificate Authority ([CA](https://en.wikipedia.org/wiki/Certificate_authority) queries the [DNS](https://en.wikipedia.org/wiki/Domain_Name_System) system to confirm that the correct TXT record exists.
+This script implements support for the Automatic Certificate Management Environment ([ACME](https://en.wikipedia.org/wiki/Automatic_Certificate_Management_Environment)) protocol [DNS-01](https://letsencrypt.org/docs/challenge-types/#dns-01-challenge) challenge. In this challenge, the domain owner creates a DNS TXT record containing a specific token for the domain. Following this, the Certificate Authority ([CA](https://en.wikipedia.org/wiki/Certificate_authority) queries the [DNS](https://en.wikipedia.org/wiki/Domain_Name_System) system to confirm that the correct TXT record exists.
 
 ## Installation
 
@@ -11,7 +11,7 @@ git clone https://github.com/contracode/cloudflare-dns-challenge-hooks.git
 ```
 
 ## Usage
-This script is used with crontab. Specify the frequency of execution through crontab.
+These scripts are used with [certbot](https://certbot.eff.org/pages/about) via crontab. Specify the frequency of execution through crontab.
 
 ```bash
 # ┌───────────── minute (0 - 59)
@@ -24,6 +24,22 @@ This script is used with crontab. Specify the frequency of execution through cro
 # │ │ │ │ │ │
 # * * * * * /bin/bash {Location of the script}
 ```
+
+## Testing
+
+Assuming that authentication details have been specified at `~/.config/cloudflare/secrets.env`:
+
+```bash
+sudo CERTBOT_DOMAIN=foo.com CERTBOT_VALIDATION=bar ./cloudflare-auth.sh
+```
+
+will create a `TXT` DNS record for `foo.com` with the name _acme-challenge_ and content, _bar_., and
+
+```bash
+sudo CERTBOT_DOMAIN=contracode.com ./cloudflare-cleanup.sh
+```
+
+will remove it.
 
 ## Tested Environments:
 Ubuntu 20.04 LTS
---
+++ cloudflare-cleanup.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+## change to "bin/sh" when necessary
+
+# Set client secrets location.
+# From: https://www.reddit.com/r/bash/comments/puujxk/comment/he7bz9s/
+API_KEY_DIR=${XDG_CONF_HOME:-$HOME/.config}/cloudflare
+
+# Create a directory for client secrets if it does not exist.
+if [ ! -d "${API_KEY_DIR}" ]; then
+    mkdir -p "${API_KEY_DIR}"
+    SECRETS_FILE=${API_KEY_DIR}/secrets.env
+
+    # Create a secrets.env file template.
+    touch ${SECRETS_FILE}
+    echo -en "#/bin/bash\n\n# These are secrets used by the 'cloudflare-ddns-updater' script.\n\n" > ${SECRETS_FILE}
+
+    echo "CLOUDFLARE_AUTH_EMAIL=\"\"                                       # The email used to login 'https://dash.cloudflare.com'" >> ${SECRETS_FILE}
+    echo "CLOUDFLARE_AUTH_METHOD=\"token\"                                 # Set to "global" for Global API Key or "token" for Scoped API Token" >> ${SECRETS_FILE}
+    echo "CLOUDFLARE_AUTH_KEY=\"\"                                         # Your API Token or Global API Key" >> ${SECRETS_FILE}
+    echo "CLOUDFLARE_ZONE_IDENTIFIER=\"\"                                  # Can be found in the "Overview" tab of your domain" >> ${SECRETS_FILE}
+    echo "CLOUDFLARE_RECORD_NAME=\"\"                                      # Which record you want to be synced (hint: the fully-qualified domain name)" >> ${SECRETS_FILE}
+    echo "CLOUDFLARE_TTL=\"3600\"                                          # Set the DNS TTL (seconds)" >> ${SECRETS_FILE}
+    echo "CLOUDFLARE_PROXY=\"false\"                                       # Set the proxy to true or false" >> ${SECRETS_FILE}
+    echo "CLOUDFLARE_SITENAME=\"\"                                         # Title of site \"Example Site\" used in Slack and/or Discord notifications" >> ${SECRETS_FILE}
+    echo "CLOUDFLARE_SLACKCHANNEL=\"\"                                     # Slack Channel #example" >> ${SECRETS_FILE}
+    echo "CLOUDFLARE_SLACKURI=\"\"                                         # URI for Slack WebHook "https://hooks.slack.com/services/xxxxx"" >> ${SECRETS_FILE}
+    echo "CLOUDFLARE_DISCORDURI=\"\"                                       # URI for Discord WebHook "https://discordapp.com/api/webhooks/xxxxx"" >> ${SECRETS_FILE}
+
+    echo "No client secrets file was found. Please fill in the values in ${SECRETS_FILE}"
+    exit
+fi
+
+SECRETS_FILE="${API_KEY_DIR}/secrets.env"
+
+# Load client secrets.
+. ${SECRETS_FILE}
+
+# Domain and challenge info from Certbot
+DOMAIN="$CERTBOT_DOMAIN"
+TXT_RECORD_NAME="_acme-challenge.${DOMAIN}"
+
+###########################################
+## Check and set the proper auth header
+###########################################
+if [[ "${CLOUDFLARE_AUTH_METHOD}" == "global" ]]; then
+  auth_header="X-Auth-Key:"
+else
+  auth_header="Authorization: Bearer"
+fi
+
+# Get the DNS record ID for the TXT record that was created
+response=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE_IDENTIFIER}/dns_records?type=TXT&name=${TXT_RECORD_NAME}" \
+  -H "X-Auth-Email: ${CLOUDFLARE_AUTH_EMAIL}" \
+  -H "$auth_header ${CLOUDFLARE_AUTH_KEY}" \
+  -H "Content-Type: application/json")
+
+# Check for errors
+case "$response" in
+*"\"success\":false"*)
+  echo -e "Cloudflare Cleanup: DNS Challenge record was not found for ${DOMAIN}. DUMPING RESULTS:\n$response" | logger -s
+  exit 1;;
+*)
+  echo -e "Cloudflare Cleanup: DNS Challenge record found for ${DOMAIN}."
+esac
+
+###########################################
+## Set the record identifier from result
+###########################################
+record_identifier=$(echo "$response" | sed -E 's/.*"id":"([A-Za-z0-9_]+)".*/\1/')
+
+response=$(curl -s -X DELETE "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE_IDENTIFIER}/dns_records/${record_identifier}" \
+  -H "X-Auth-Email: ${CLOUDFLARE_AUTH_EMAIL}" \
+  -H "$auth_header ${CLOUDFLARE_AUTH_KEY}" \
+  -H "Content-Type: application/json")
+
+# Check for errors
+case "$response" in
+*"\"success\":false"*)
+  echo -e "Cloudflare Cleanup: DNS Challenge record cleanup failed for ${DOMAIN}. DUMPING RESULTS:\n$response" | logger -s
+  exit 1;;
+*)
+  echo -e "Cloudflare Cleanup: DNS Challenge record cleanup successful for ${DOMAIN}."
+  exit 0;;
+esac
+
